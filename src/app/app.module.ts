00caee15eec0853d9647c9b35fa3a501538b389a
najahAccelex <najah@accelextech.com>
2023-08-29 11:40:33 +0100

    add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        BrowserAnimationsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
