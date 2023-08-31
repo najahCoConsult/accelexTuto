7dae69996e0b133ac5a7f9b5305499e0eb61ca25
najahAccelex <najah@accelextech.com>
2023-08-31 13:58:29 +0100

    display list of products ByCategory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsListPageComponent} from "./components/products-list-page/products-list-page.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductsListPageComponent,
+  },
+  {
+    path: ':category',
+    component: ProductsListPageComponent,
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/components/products-list-page/products-list-page.component.ts
+++ src/app/components/products-list-page/products-list-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Product} from "../../models/products.model";
 import {ShopService} from "../../services/shop.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-products-list-page',
@@ -9,12 +10,24 @@ import {ShopService} from "../../services/shop.service";
 })
 export class ProductsListPageComponent implements OnInit {
 
-  constructor(private shopService: ShopService) {}
+  constructor(private shopService: ShopService, private readonly route: ActivatedRoute) {}
   products: Product[] = [];
-
+  url: string = 'category/';
+  category: string = '';
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe((products) => {
-      this.products = products;
+    this.route.params.subscribe((params) => {
+      if (params['category']) {
+        this.category = this.url + params['category'];
+        this.shopService.getProductByCategory(this.category).subscribe((products) => {
+          this.products = products;
+          console.log(this.products);
+        });
+      } else {
+        this.shopService.getProductByCategory(this.category).subscribe((products) => {
+          this.products = products;
+          console.log(this.products);
+        });
+      }
     });
   }
 
