d0274e991eebd4d76d38a8e1c270c738889c436f
najahAccelex <najah@accelextech.com>
2023-09-05 14:52:35 +0100

    create ProductHeadingPipe

---
+++ src/app/pipes/product-heading.pipe.spec.ts
@@ -0,0 +1,8 @@
+import {ProductHeadingPipe} from './product-heading.pipe';
+
+describe('ProductHeadingPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ProductHeadingPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
